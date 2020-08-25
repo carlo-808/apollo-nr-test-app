7240adebdd9c539923da4dd4bd33f9c8a890a0f7
Carlo Pearson <cpearson@newrelic.com>
2020-08-25 15:20:47 -0700

    add deepest path

--- apollo_server/src/plugins/plugin.js
+++ apollo_server/src/plugins/plugin.js
@@ -1,76 +1,111 @@
 'use strict'
 
+function namesAndDepths(nodes, depth = 1) {
+  const childNodes = nodes.map(node => {
+    if (node.kind === 'OperationDefinition' || node.kind === 'Field') {
+      const name = node.name ? node.name.value : 'query'
+      const children = node.selectionSet ? node.selectionSet.selections : null
+
+      // The "leaf" of the branch. This can return multiple names/depths, like
+      // when a query has many fields being pulled for an object type
+      if (!children) {
+        return { name, depth }
+      }
+
+      const childNamesAndDepths = namesAndDepths(children, depth + 1).map(nameAndDepth => {
+        nameAndDepth.name = `${name}.${nameAndDepth.name}`
+        return nameAndDepth
+      })
+
+      return childNamesAndDepths
+    }
+
+    // TODO: what else is there??
+  })
+
+  return childNodes.flat()
+}
+
+function deepestPath(context) {
+  const definitions = context.document.definitions
+  const names = namesAndDepths(definitions)
+  const deepestChild = names.reduce((deepest, current) => {
+    if (current.depth > deepest.depth) {
+      return current
+    }
+    return deepest
+  })
+
+  return deepestChild
+}
+
 const shimPlugin = (options) => {
   const nr = options.newrelic
   return {
-      requestDidStart(context) {
-        const shim = nr.shim
-        const segmentMap = new Map()
+    requestDidStart(context) {
+      const shim = nr.shim
+      const segmentMap = new Map()
 
-        const segmento = nr.agent.tracer.getSegment()
-        segmento.name = `Operation: ${context.operationName}`
-        const sego = {
-          segment: segmento,
-          name: segmento.name
-        }
+      const segmento = nr.agent.tracer.getSegment()
+      segmento.name = `Operation: ${context.operationName}`
+      const sego = {
+        segment: segmento,
+        name: segmento.name
+      }
 
-        shim.setActiveSegment(segmento)
+      shim.setActiveSegment(segmento)
 
-        segmentMap.set('root', sego)
+      segmentMap.set('root', sego)
 
-        return {
-          didResolveOperation (context) {
-            // console.log('>>>>>>>>>>> ', context.operationName)
-            const rootSeg = segmentMap.get('root').segment
+      return {
+        didResolveOperation(context) {
+          let name = context.operationName
+          if (!name) {
+            name = deepestPath(context).name
+          }
 
-            rootSeg.name 
-              = `Operation: ${context.operationName}`
+          const rootSeg = segmentMap.get('root').segment
+          rootSeg.name = `Operation: ${name}`
+          nr.setTransactionName(`Operation: ${name}`)
+        },
+        executionDidStart: () => ({
+          willResolveField({info}) {
+            if (isQueryOrMutation(info.parentType)) {
+              // get root segment to add as parent to Query segment
+              const rootSeg = segmentMap.get('root').segment
+              const name = `${info.parentType}: ${info.fieldName}`
 
-            nr.setTransactionName(`Operation: ${context.operationName}`)
-          },
-          executionDidStart: () => ({
-            willResolveField({source, info, context}) {
-              if (isQueryOrMutation(info.parentType)) {
-                // get root segment to add as parent to Query segment
-                const rootSeg = segmentMap.get('root').segment
-                const name = `${info.parentType}: ${info.fieldName}`
-
-                const seg = {
-                  segment: shim.createSegment(name, rootSeg),
-                  name: info.fieldName
-                }
-                
-                seg.segment.start()
-                shim.setActiveSegment(seg.segment)
-
-                segmentMap.set(info.fieldName, seg)
+              const seg = {
+                segment: shim.createSegment(name, rootSeg),
+                name: info.fieldName
               }
 
-              return () => {
-                if (isQueryOrMutation(arguments[0].info.parentType)) {
-                  const seg = segmentMap.get(info.fieldName).segment
-  
-                  if (seg) {
-                    seg.end()
-                    const rootSeg = segmentMap.get('root').segment
-                    shim.setActiveSegment(rootSeg)
-                  }
+              seg.segment.start()
+              shim.setActiveSegment(seg.segment)
+
+              segmentMap.set(info.fieldName, seg)
+            }
+
+            return () => {
+              if (isQueryOrMutation(arguments[0].info.parentType)) {
+                const seg = segmentMap.get(info.fieldName).segment
+
+                if (seg) {
+                  seg.end()
+                  const rootSeg = segmentMap.get('root').segment
+                  shim.setActiveSegment(rootSeg)
                 }
               }
             }
-          }),
-          willSendResponse (context) {
-            // This is not getting the right segment
-            // const segmento = nr.agent.tracer.getSegment()
-            // segmento.name = `Operation: ` + context.operationName
           }
-        }
+        })
+      }
     }
   }
 }
 
 const isQueryOrMutation = (type) => {
-  return type + '' === 'Query' || type + '' === 'Mutation' 
+  return type + '' === 'Query' || type + '' === 'Mutation'
 }
 
 module.exports = shimPlugin
